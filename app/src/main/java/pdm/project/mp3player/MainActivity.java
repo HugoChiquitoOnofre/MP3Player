454ed75b95d9c58d976567ea1be5dc49d33f6127
Cershi <hugo.chiquito.onofre@outlook.com>
2022-10-02 18:39:02 -0500

    Initial commit

---
+++ app/src/main/java/pdm/project/mp3player/MainActivity.java
@@ -0,0 +1,85 @@
+package pdm.project.mp3player;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.ArrayList;
+
+import pdm.project.mp3player.databinding.ActivityMainBinding;
+import pdm.project.mp3player.model.MusicFiles;
+import pdm.project.mp3player.provider.MusicStore;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    //PERMISSION
+    public static final int REQUEST_CODE = 1;
+
+    //MP3
+    ArrayList<MusicFiles> musicFiles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        permission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    musicFiles = MusicStore.getAllAudio(this);
+                    initViewPager();
+                }
+                else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                            , REQUEST_CODE);
+                }
+            }
+        }
+    }
+
+    private void permission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                    , REQUEST_CODE);
+        }
+        else {
+            musicFiles = MusicStore.getAllAudio(this);
+            initViewPager();
+        }
+    }
+
+    private void initViewPager() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_library)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
---
+++ app/src/main/java/pdm/project/mp3player/provider/MusicStore.java
@@ -0,0 +1,44 @@
+package pdm.project.mp3player.provider;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import pdm.project.mp3player.model.MusicFiles;
+
+public class MusicStore {
+    private static ArrayList<MusicFiles> audioList;
+    private static Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+    public static ArrayList<MusicFiles> getAllAudio(Context context) {
+        audioList = new ArrayList<>();
+        String[] projection = {
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST,
+        };
+        Cursor cursor = context.getContentResolver().query(uri, projection,
+                null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String album = cursor.getString(0);
+                String title = cursor.getString(1);
+                String duration = cursor.getString(2);
+                String path = cursor.getString(3);
+                String artist = cursor.getString(4);
+
+                MusicFiles musicFiles = new MusicFiles(path, title, artist, album, duration);
+                Log.e("Path:" + path, "Album" + album);
+                audioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return audioList;
+    }
+}
